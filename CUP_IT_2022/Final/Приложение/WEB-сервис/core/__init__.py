deb0060d30842a6e7a1bfdf5107d5338c6555feb
Shamsutdinov Vadim <72353397+pinacoladio@users.noreply.github.com>
2022-04-18 21:18:36 +0300

    Delete CUP_IT_2022/Final directory

--- CUP_IT_2022/Final/Приложение/WEB-сервис/core/__init__.py
+++
@@ -1,2 +0,0 @@
-from .config import APP_HOST, APP_PORT, IS_DEV, PSQL_URL
-from .assets import lossForAllSubdivisionsQuery
--- CUP_IT_2022/Final/Приложение/WEB-сервис/db/__init__.py
+++
@@ -1,4 +0,0 @@
-from statistics import mode
-from .setup import getDb
-from .models import WorkTime, Nodes, Service, Employee
-from .schemas import *
